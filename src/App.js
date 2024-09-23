0116d5c255ffa7ac3ff8e9620f46aca46fb8d465
vk-codes24x7 <vinayakrishna1001@gmail.com>
2024-09-23 08:15:36 -0400

    added the calendar, form and invoices page along with necessary dependencies

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ import { ColorModeContext, useMode } from "./theme";
 import { Route, Routes } from "react-router-dom";
 import Team from "./pages/team";
 import Contacts from "./pages/contacts";
+import Invoices from "./pages/invoices";
+import Form from "./pages/form";
+import Calendar from "./pages/calendar";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -24,6 +27,9 @@ function App() {
               <Route path="/" element={<Dashboard />} />
               <Route path="/team" element={<Team />} />
               <Route path="/contacts" element={<Contacts />} />
+              <Route path="/invoices" element={<Invoices />} />
+              <Route path="/form" element={<Form />} />
+              <Route path="/calendar" element={<Calendar />} />
             </Routes>
           </main>
         </div>
